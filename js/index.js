df6aff6fec971e1820a65350c929b4afc981335b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-12 15:25:02 +0200

    Added price for kilometrs and coments section

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 // Consolore Loader
 console.log("JS Biglietto Treno Loaded");
 
+// User Inputs
 const userDistance = parseInt
     (prompt("Indica il numero di chilometri che vuoi percorrere:"));
 const userAge = parseInt
     (prompt("Indica la tua eta:"));
+
+// Casting Variables
+let priceTicket = 0.21;
+
+// Discount Cycle for Minors
+
+// Discount Cycle for Over 65 Age
