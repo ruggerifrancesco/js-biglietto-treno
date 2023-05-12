44fad74d9e3e6e2ada10901e2fd8aaef57a70d82
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-12 15:18:38 +0200

    Added firsdt prompts for user get asked infos

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // Consolore Loader
 console.log("JS Biglietto Treno Loaded");
+
+const userDistance = parseInt
+    (prompt("Indica il numero di chilometri che vuoi percorrere:"));
+const userAge = parseInt
+    (prompt("Indica la tua eta:"));
