cfef1d70775a414c96760eb681dcc21f9ae51c8f
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-12 16:07:44 +0200

    calculated price of kilometers

--- js/index.js
+++ js/index.js
@@ -2,20 +2,21 @@
 console.log("JS Biglietto Treno Loaded");
 
 // User Inputs
-const userDistance = parseInt
+let userDistance = parseInt
     (prompt("Indica il numero di chilometri che vuoi percorrere:"));
 const userAge = parseInt
     (prompt("Indica la tua eta:"));
 
 // Casting Variables
-let priceTicket = 0.21;
+let priceTicket= userDistance * 0.21;
+
+let priceTicketResult;
+
 
 // Discount Cycle for Minors
 if (userAge < 18) {
-
 }
 
 // Discount Cycle for Over 65 
 if (userAge >= 65){
-
 }
